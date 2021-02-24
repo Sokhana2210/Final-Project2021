85ca8bbae66e2f5e9e2359d346d69ff690913e60
Sokhana2210 <sokhanalingndzube@gmail.com>
2021-02-24 16:28:42 +0200

    started backend

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import sqlite3
 from sqlite3 import Error
 
+def create_connection(db_file):
+    """create a database connection to a SQLite database"""
 
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+if __name__ == '_main':
+    create_connection(r"C:\sqlite\db\pythonsqlite.db")
 
-# def create_connection(db_file):
-#     """create a database connection to a SQLite database"""
-#
-#     conn = None
-#     try:
-#         conn = sqlite3.connect(db_file)
-#         print(sqlite3.version)
-#     except Error as e:
-#         print(e)
-#     finally:
-#         if conn:
-#             conn.close()
-# if __name__ == '_main':
-#     create_connection(r"C:\sqlite\db\pythonsqlite.db")
-#
-# /home/specialuser/PycharmProjects/pythonProject5
