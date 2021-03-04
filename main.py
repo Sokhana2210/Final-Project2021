685280d6dd1dc2d1f975b7caed59bd742a0c64c4
Sokhana2210 <sokhanalingndzube@gmail.com>
2021-03-04 16:11:18 +0200

    Updated

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import sqlite3
-from flask import Flask, request, jsonify, app
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
 
 
 def init_sqlite_db():
@@ -16,51 +13,82 @@ def init_sqlite_db():
 
     conn.execute('CREATE TABLE IF NOT EXISTS registered_users(user id INTEGER, firstname TEXT, lastname TEXT,username TEXT, phone_number INTEGER, password TEXT)')
     print("Table 2 was created")
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM bookers ")
+
+    print(cursor.fetchall())
+
     conn.close()
-
-
-def dict_factory(cursor,row):
-    d = {}
-    for idx,col in enumerate(cursor.discription):
-        d[col[0]] = row[idx]
-    return d
 
 
 init_sqlite_db()
 
+app = Flask(__name__)
+CORS(app)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.discription):
+        d[col[0]] = row[idx]
+    return d
+
 
 @app.route('/')
-@app.route('/insert/',methods = ['POST'])
-
-def bookers ():
-
-    if request.method =='POST':
+@app.route('/insert/', methods=['POST'])
+def bookers():
+    msg = None
+    if request.method == "POST":
+        msg = None
         try:
+            post_data = request.get_json()
+            firstname = post_data['firstname']
+            lastname = post_data['lastname']
+            phone_no = post_data['phoneNo']
+            categ = post_data['cate']
 
-
-          firstname = request.form['fname']
-          lastname = request.form['lname']
-          phone_number = request.form['phone-No']
-          category = request.form['cate']
-
-
-          if firstname == firstname:
             with sqlite3.connect('pearlsN&B.db') as con:
-                cursor =con.cursor()
-                cursor.execute("INSERT INTO bookers(firstname,lastname,phone_number, category)", firstname,lastname,phone_number,category)
+                cursor = con.cursor()
+                cursor.execute("INSERT INTO bookers(firstname,lastname,phone_number, category)", firstname, lastname, phone_no, categ)
                 con.commit()
-                msg = firstname + lastname +"was inserted to the database"
+                msg = firstname + lastname + "was inserted to the database"
 
         except Exception as e:
-            msg  = "Error occured"+ str(e)
-            con.rollback()
+            msg = "Error occured" + str(e)
 
         finally:
-            con.close()
-        return jsonify(msg=msg)
+            return {'msg': msg}
 
 
-@app.route('/show-bookers/', methods=['GET'])
+@app.route('/')
+@app.route('/place/', methods=['POST'])
+def registered_users():
+    msg = None
+    if request.method == "POST":
+        msg = None
+        try:
+            post_data = request.get_json()
+            firstname = post_data['firstname']
+            lastname = post_data['lastname']
+            username = post_data['username']
+            phone_no = post_data['phoneNo']
+            password = post_data['password']
+
+            with sqlite3.connect('pearlsN&B.db') as con:
+                cursor = con.cursor()
+                cursor.execute("INSERT INTO registered_users(firstname,lastname,phone_number,)", (firstname, lastname, username, phone_no, password))
+                con.commit()
+                msg = firstname + lastname + "was inserted to the database"
+
+        except Exception as e:
+            msg = "Error occured" + str(e)
+
+        finally:
+            return {'msg': msg}
+
+
+@app.route('/show-registered_users/', methods=['GET'])
 def show():
     users = []
     try:
